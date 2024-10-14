9ceee46f1d3f0fb416b5a03fc6186902a3c80e72
decalcomania22 <abhilashapark22@gmail.com>
2024-10-14 16:53:00 +0530

    Add footer

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -6,16 +6,18 @@ import RecentProject from "./components/RecentProject";
 import { navItems } from "./data";
 import Clients from "./components/Clients";
 import Experience from "./components/Experience";
+import Footer from "./components/Footer";
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems}/>
         <Hero/>
         <Grid/>
         <RecentProject/> 
         <Clients/>
-        <Experience/>
+        {/* <Experience/> */}
+        <Footer/>
       </div>
     </main>
   );
