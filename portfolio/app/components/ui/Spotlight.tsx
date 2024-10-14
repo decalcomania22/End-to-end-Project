3e6a3e3b939a39a987774d8e1200c2e64b8e4d04
decalcomania22 <abhilashapark22@gmail.com>
2024-10-14 11:42:06 +0530

    Add testimonials

--- portfolio/app/components/ui/Spotlight.tsx
+++ portfolio/app/components/ui/Spotlight.tsx
@@ -1,10 +1,11 @@
+"use client";
 import React from "react";
 import { cn } from "@/lib/utils";
 
 type SpotlightProps = {
   className?: string;
   fill?: string;
-};
+}; 
 
 export const Spotlight = ({ className, fill }: SpotlightProps) => {
   return (
--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
 import Hero from "./components/Hero";
 import { FloatingNav } from "./components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 import Grid from "./components/Grid";
+import RecentProject from "./components/RecentProject";
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
@@ -12,6 +12,7 @@ export default function Home() {
         ]}/>
         <Hero/>
         <Grid/>
+        <RecentProject/>
       </div>
     </main>
   );
